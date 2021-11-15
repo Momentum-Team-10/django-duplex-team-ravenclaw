3f340d05ea8bd236eb72e03ef444b3bf530329f8
James Moore <james816am@gmail.com>
2021-11-15 15:32:16 -0500

    Fixed caching problem, gitignore should work

--- flashcards/views.py
+++ flashcards/views.py
@@ -54,7 +54,7 @@ def add_card(request, pk):
         form = CardForm(data=request.POST)
         if form.is_valid():
             card = form.save(commit=False)
-            card.user_id = deck.pk
+            card.deck_id = pk
             card.save()
             return redirect(to='view_cards', pk=pk)
 
