77d10f499cf03a82ea266e526ac3c024ae354616
Zack Robertshaw <zackrobertshaw@gmail.com>
2021-11-17 10:47:25 -0500

    added counter and buttons to play_cards

--- static/JS/script.js
+++ static/JS/script.js
@@ -14,4 +14,24 @@ for (let card of cards) {
 // 	item.classList.toggle("flipcard");
 // }
 
+// Below is the counter action
+var wrong = (function () {
+    var counter = 0;
+    return function () {return counter += 1;}
+  })();
+
+  var right = (function () {
+    var counter = 0;
+    return function () {return counter += 1;}
+  })();
+
+  
+  function wrongAnswer(){
+    document.getElementById("wrong").innerHTML = wrong();
+  }
+
+  function rightAnswer(){
+    document.getElementById("right").innerHTML = right();
+  }
+
 
